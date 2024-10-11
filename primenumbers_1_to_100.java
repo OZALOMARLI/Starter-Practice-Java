6b67dd7844cf42237b8f54a85a7b3fd82d06d458
OZAL OMARLI <130607739+OZALOMARLI@users.noreply.github.com>
2024-10-11 15:29:38 +0300

    Update and rename primenumber_1_to_100.java to primenumbers_1_to_100.java

--- primenumbers_1_to_100.java
+++ primenumbers_1_to_100.java
@@ -1,6 +1,6 @@
 // Application that prints prime numbers from 1 to 100
 
-public class primenumber_1_to_100 {
+public class primenumbers_1_to_100 {
     public static void main(String[] args) {
         System.out.println("Prime Numbers in 1 to 100 :");
         for (int i = 1; i<=100; i++) {

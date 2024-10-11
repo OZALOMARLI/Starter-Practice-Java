3dea55984f6883f061305e83683c722f28fa5b45
OZAL OMARLI <130607739+OZALOMARLI@users.noreply.github.com>
2024-10-11 15:12:41 +0300

    Add files via upload

---
+++ primenumber_1_to_100.java
@@ -0,0 +1,18 @@
+public class primenumber_1_to_100 {
+    public static void main(String[] args) {
+        System.out.println("Prime Numbers in 1 to 100 :");
+        for (int i = 1; i<=100; i++) {
+            int count =0;
+        for (int isprime=1; isprime < i; isprime++) {
+            if (i%isprime == 0){
+                count++;
+            }
+
+        }
+            if(count==1){
+                System.out.println (" Prime Numbers :"+i);
+            }
+        }
+
+    }
+}
